d0a30538d6d85491418e92098c833a042bf29e2a
Yuki Nakato <crystal.icicle@gmail.com>
2022-01-29 01:02:25 +0900

    first commit

---
+++ GetEnglishFontNames.cs
@@ -0,0 +1,13 @@
+﻿class GetEnglishFontNames
+{
+    static void Main()
+    {
+        var col = new System.Drawing.Text.InstalledFontCollection();
+        var fonts = col.Families;
+        var lcid = System.Globalization.CultureInfo.GetCultureInfo("en-US").LCID;
+        foreach (var font in fonts)
+        {
+            System.Console.WriteLine(font.GetName(lcid));
+        }
+    }
+}
---
+++ GetEnglishFontNames.ps1
@@ -0,0 +1,4 @@
+﻿$col = New-Object System.Drawing.Text.InstalledFontCollection
+$fonts = $col.Families
+$lcid = (Get-Culture en-US).lcid
+foreach ($font in $fonts) { Write-Output $font.GetName($lcid) }
